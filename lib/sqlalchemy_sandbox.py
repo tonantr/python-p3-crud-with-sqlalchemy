483d482cb71d72b88fc7cfeee1125e359be11063
tonan <tonantruong@hotmail.com>
2023-08-11 19:23:00 -0400

    crud with sqlalchemy

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -13,9 +13,82 @@ Base = declarative_base()
 class Student(Base):
     __tablename__ = 'students'
 
+    Index('index_name', 'name')
+
     id = Column(Integer(), primary_key=True)
     name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
+    
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    albert_einstein = Student(
+        name="Albert Einstein",
+        email="albert.einstein@zurich.edu",
+        grade=6,
+        birthday=datetime(
+            year=1879,
+            month=3,
+            day=14
+        ),
+    )
+
+    alan_turing = Student(
+        name="Alan Turing",
+        email="alan.turing@sherborne.edu",
+        grade=11,
+        birthday=datetime(
+            year=1912,
+            month=6,
+            day=23
+        ),
+    )
+
+    # session.add(albert_einstein)
+    # session.commit()
+
+    # print(f"New student ID is {albert_einstein.id}.")
+
+    session.bulk_save_objects([albert_einstein, alan_turing])
+    session.commit()
+
+    # students = session.query(Student).all()
+
+    # print(students)
+
+    # names = session.query(Student.name).all()
+
+    # print(names)
+
+    # students_by_name = session.query(
+    #         Student.name).order_by(
+    #         Student.name).all()
+
+    # print(students_by_name)
+
+    # students_by_grade_desc = session.query(
+    #         Student.name, Student.grade).order_by(
+    #         desc(Student.grade)).all()
+
+    # print(students_by_grade_desc)
+
+    oldest_student = session.query(
+            Student.name, Student.birthday).order_by(
+            desc(Student.grade)).limit(1).all()
+
+    print(oldest_student)
+
+    
